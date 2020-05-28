53dc3a50a85e616ac69dbdcb6859447c122efa32
wuding <netjoin@gmail.com>
2020-05-29 03:47:32 +0800

    新增配置文件分发，支持转发路由文件

--- web/index.php
+++ web/index.php
@@ -16,6 +16,8 @@
 **/
 defined('REQUEST_NAME') ? : define('REQUEST_NAME', parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 defined('BASE_DIR') ? : define('BASE_DIR', dirname(__DIR__));
+$_CONFIG = include BASE_DIR . '/app/config.php';
+$rewrite_file = $_CONFIG['rewrite_file'] ?? null;
 
 $sorting_order = isset($_GET['sort']) ? $_GET['sort'] : null;
 $request_urn = preg_replace('/^\/|\.(html|htm|php)$/i', '', REQUEST_NAME);
@@ -24,15 +26,19 @@ $end_filename = BASE_DIR . '/app/' . trim($request_urn, '/') . '.php';
 $directory = is_dir($request_filename) ? $request_filename : false;
 
 if (!$request_urn || preg_match('/^index$/i', $request_urn)) {
-	include BASE_DIR . '/web/index.html';
+    include BASE_DIR . '/web/index.html';
 } elseif ($directory) {
-	include BASE_DIR . '/src/scandir.php';
+    include BASE_DIR . '/src/scandir.php';
 } elseif (file_exists($request_filename)) {
-	include $request_filename;
+    include $request_filename;
 } elseif (!file_exists($end_filename)) {
-	# print_r(get_defined_vars());exit;
-	include BASE_DIR . '/app/template/404.html';
+    # print_r(get_defined_vars());exit;
+    if ($rewrite_file) {
+        include $rewrite_file;
+    } else {
+        include BASE_DIR . '/app/template/404.html';
+    }
 } else {
-	include $end_filename;
+    include $end_filename;
 }
 
