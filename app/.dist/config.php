2e6d92d5d99ffd12b50dfb513e7d0c006bb27224
wuding <netjoin@gmail.com>
2020-05-29 20:38:20 +0800

    使用 .dist 文件夹放分发文件

---
+++ app/.dist/config.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+    # 'rewrite_file' => 'I:/env/www/rewrite.php',
+    'rewrite_file' => BASE_DIR . '/web/rewrite.php',
+    'example' => 'magic-cube/index',
+    'uri_custom' => '',
+    # 'uri_custom' => 'get,post:users!wubenli@documents$1%2Fview', //$1
+    'virtual_paths' => array(),
+    'virtual_hosts' => array(),
+    'host_domain' => '.loc.urlnk.com',
+    'database' => array(
+        'db_name' => '',
+        'username' => 'root',
+        'password' => 'root',
+    ),
+);
