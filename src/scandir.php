9be9cae76bf246a921d11456ebc7e8789a3d6475
netjoin <netjoin@gmail.com>
2020-06-02 18:45:38 +0800

    添加文件检测统计，并复制到指定目录

--- src/scandir.php
+++ src/scandir.php
@@ -9,10 +9,40 @@ $files = scandir($directory ? : '.', $sorting_order ? 1 : 0);
 unset($files[array_search('.', $files)]);
 unset($files[array_search('..', $files)]);
 $dir = trim(REQUEST_NAME, '/');
-$dir = $dir ? '/' . $dir . '/' : '';
-$lines = array("<a href=\"$dir..\">..</a>");
+$path = $dir ? '/' . $dir . '/' : '';
+$lines = array("<a href=\"$path..\">..</a>");
+$arr = [];
 foreach ($files as $file) {
-    $lines[] = "<a href=\"$dir$file\">$file</a>";
+    $copy = null;
+    $url = $path . $file;
+    $filename = $dir .'/'. $file;
+    $lines[] = "<a href=\"$url\">$file</a>";
+
+    /*
+    // 文件检测统计，并复制到指定目录
+    $target = "D:/Setup/Fonts/ttf/$file";
+    $exists = file_exists($target);
+    $stat = stat($filename);
+    foreach ($stat as $key => $value) {
+        if (is_numeric($key)) {
+            unset($stat[$key]);
+        }
+    }
+    if (!$exists) {
+        $copy = copy($filename, $target);
+    }
+    $arr[] = array(
+        'filename' => $filename,
+        'stat' => $stat,
+        'atime' => date('Y-m-d H:i:s', $stat['atime']),
+        'ctime' => date('Y-m-d H:i:s', $stat['ctime']),
+        'mtime' => date('Y-m-d H:i:s', $stat['mtime']),
+        'exists' => $exists,
+        'copy' => $copy,
+    );
+    */
 }
 $html = '<li>' . implode('</li><li>', $lines) . '</li>';
 echo $html;
+# print_r(json_encode($arr));
+# echo date ("Y-m-d H:i:s", getlastmod());
