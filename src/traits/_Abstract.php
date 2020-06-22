1144519641c0b0d254afc719d2960034507d95ea
wuding <netjoin@gmail.com>
2020-06-22 10:51:12 +0800

    添加机器人接口标准输出方法

--- src/traits/_Abstract.php
+++ src/traits/_Abstract.php
@@ -7,4 +7,14 @@ trait _Abstract
     {
         print_r(func_get_args());
     }
+
+    public static function _outputJson($code = null, $msg = null, $data = null)
+    {
+        $arr = [
+            'code' => $code,
+            'msg' => $msg,
+            'data' => $data
+        ];
+        return json_encode($arr);
+    }
 }
