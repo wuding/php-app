8f232a6efde33a74d93a81944dc532719437aa3e
wuding <netjoin@gmail.com>
2020-06-26 17:51:08 +0800

    添加别名、MV、歌手、个人主页、头像等视图方法

--- src/traits/_Abstract.php
+++ src/traits/_Abstract.php
@@ -22,4 +22,46 @@ trait _Abstract
     {
         return $str = date('i:s', $str / 1000);
     }
+
+    public static function alias($str)
+    {
+        $arr = (array) json_decode($str);
+        $str = $arr[0] ?? null;
+        return $str ? " &nbsp; $str" : null;
+    }
+
+    public static function mv($mv)
+    {
+        return $str = $mv ? "<a href=\"https://music.163.com/mv?id=$mv\">▶</a>" : null;
+    }
+
+    public static function artists($artists)
+    {
+        $arr = explode(',', $artists);
+        $pieces = [];
+        foreach ($arr as $key => $value) {
+            if ($value) {
+                $row = self::$artists[$value] ?? null;
+                if ($row) {
+                    $pieces[] = "<a href=\"/music/artist/$value/site/1\">$row->name</a>";
+                }
+            }
+        }
+        return implode(' / ', $pieces);
+    }
+
+    public static function home($home)
+    {
+        return $str = $home ? "<a href=\"https://music.163.com/user/home?id=$home\">$home</a>" : null;
+    }
+
+    public static function publishTime($publishTime)
+    {
+        return $str = $publishTime ? date('Y-m-d H:i:s', $publishTime / 1000) : null;
+    }
+
+    public static function pic($pic)
+    {
+        return $str = $pic ? "<img style=\"width:24px\" src=\"$pic?param=24y24\">" : null;
+    }
 }
