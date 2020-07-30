39319ae863d582690d9797e7415dbf4db59d62e7
wuding <netjoin@gmail.com>
2020-07-31 07:37:35 +0800

    缩进

--- app/route.php
+++ app/route.php
@@ -1,18 +1,18 @@
 <?php
 return [
-	# 'name' => 'nikic/fast-route',
-	'name' => 'wuding/equiv-route',
-	'routes' => [
-		[['POST', 'GET'], '/admin/user[/{id:\d+}[/{name}]]', 'get_user_handler'],
-		['GET', '/articles/{id:\d+}[/{title}]', 'get_article_handler'],
-		['GET', '/user/{name}', 'user_name_handler'],
-		['POST', '/users'],
-		['GET', '/play/{id}', 'get,post:play!index@index'],
-		['GET', '^/{template:\d+}-{param}', 'url!redirect@index'],
-	],
-	'options' =>  [
-	    'cacheFile' => __DIR__ . '/../storage/cache/route.cache',
-	    'cacheDisabled' => 1,
-	],
-	'status' => 1,
+    # 'name' => 'nikic/fast-route',
+    'name' => 'wuding/equiv-route',
+    'routes' => [
+        [['POST', 'GET'], '/admin/user[/{id:\d+}[/{name}]]', 'get_user_handler'],
+        ['GET', '/articles/{id:\d+}[/{title}]', 'get_article_handler'],
+        ['GET', '/user/{name}', 'user_name_handler'],
+        ['POST', '/users'],
+        ['GET', '/play/{id}', 'get,post:play!index@index'],
+        ['GET', '^/{template:\d+}-{param}', 'url!redirect@index'],
+    ],
+    'options' =>  [
+        'cacheFile' => __DIR__ . '/../storage/cache/route.cache',
+        'cacheDisabled' => 1,
+    ],
+    'status' => 1,
 ];
