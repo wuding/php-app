9bc7f6fc8cefc2ab050b9936341093754f7d9dc6
wuding <netjoin@gmail.com>
2020-07-31 03:57:20 +0800

    调试信息加入包含的文件列表

--- example/magic-cube/index.php
+++ example/magic-cube/index.php
@@ -6,6 +6,7 @@ use MagicCube\Dispatcher;
 
 class Index
 {
+    const VERSION = '20.213.237';
     public $dispatcher = null;
 
     public function __construct($routeInfo, $httpMethod)
@@ -31,3 +32,29 @@ $result = $index->dispatch($_CONFIG['debug']);
 if ($_CONFIG['debug']) {
     print_r(array($result, __FILE__, __LINE__));
 }
+
+if (null !== $_CONFIG['debug']) {
+    $files = get_included_files();
+    // 合并漏掉的
+    extract(\Ext\Yac::hash('files', 'files_', 1));
+    foreach ($cacheValue as $key => $value) {
+        if (!in_array($value, $files)) {
+            $files[] = $value;
+        }
+    }
+    // 排序
+    if (false === $_CONFIG['debug']) {
+        sort($files);
+    }
+    print_r(array($files, __FILE__, __LINE__));
+}
+
+/* vendor/composer/ClassLoader.php findFile()
+extract(Yac::hash('files', 'files_', 1));
+$arr = [];
+if (false !== $cacheValue) {
+    $arr = (array) $cacheValue;
+}
+$arr[$class] = realpath($file);
+Yac::store($cacheKey, $arr, 30, 1);
+*/
