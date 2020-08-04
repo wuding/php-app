6e0ae047bf1238ed7933dcfb23b678e3a9fb2503
wuding <netjoin@gmail.com>
2020-08-04 19:24:11 +0800

    添加输出回调函数读写 gz 格式

--- src/view/Callback.php
+++ src/view/Callback.php
@@ -1,6 +1,10 @@
 <?php
 namespace view;
 
+use Ext\File;
+use Ext\Zlib;
+use model\Glob;
+
 /**
  * 模板引擎输出回调类
  */
@@ -15,5 +19,20 @@ class Callback
     {
       // 不能返回数组或对象，也不能直接输出
       return print_r(array('file' => __FILE__, 'buffer' => $buffer), true);
+    }
+
+    // 写入
+    public static function gz($buffer)
+    {
+        $filename = Glob::conf('outputCallback.gz');
+        return Zlib::putContents($filename, $buffer);
+    }
+
+    // 读取，另存为 .zip 7-Zip 打开才正常
+    public static function gzip($buffer)
+    {
+        $filename = Glob::conf('outputCallback.gz');
+        $str = File::getContents(realpath($filename));
+        return $str;
     }
 }
