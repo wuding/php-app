f0b17cf253083ad7cf287742a3128fe25c26d2d5
wuding <netjoin@gmail.com>
2020-08-12 22:38:56 +0800

    添加会话

--- example/magic-cube/index.php
+++ example/magic-cube/index.php
@@ -3,6 +3,7 @@
 namespace PhpApp\Example;
 
 use Stat;
+use Ext\X\PhpRedis;
 use MagicCube\Dispatcher;
 use model\Glob;
 
@@ -35,24 +36,41 @@ $var_stat = $_GET['stat'] ?? null;
 if (null !== $var_stat) {
     $expire = time() + 864000000;
     $setcookie = setcookie('stat', $var_stat, $expire, '/');
+} else {
+    $var_stat = Glob::conf('stat');
 }
 $disable_stat = $_COOKIE['stat'] ?? $var_stat;
+
 $host_string = preg_replace("/\.|:/", '_', $_SERVER['HTTP_HOST'] ?? 'err');
 $host_name = strtoupper($host_string);
 $stat = [];
+
+// 会话开始
+$sname = Glob::conf('session.name');
+$sid = $_GET['sid'] ?? null;
+Stat::$unique = md5(json_encode($_SERVER));
+Glob::$sid = $_COOKIE[$sname] ?? ($sid ?: Stat::$unique);
+session_name($sname);
+session_id(Glob::$sid);
+session_start();
+$_SESSION['updated'] = time();
+
 // 排除统计
 $request_path = parse_url($uri, PHP_URL_PATH);
 if (!preg_match("/^\/(stat|robot)(|\/.*)$/i", $request_path) && !$disable_stat) {
-    $stat['server'] = Stat::server();
-    Glob::diff('STAT_SERVER');
-    $stat['url'] = Stat::record();
+    PhpRedis::select(Glob::conf('redis.stat_dbindex'));
+    PhpRedis::sAdd("stat_session", Glob::$sid);
+    $stat['srv'] = Stat::srv();
+    Glob::diff('STAT_SRV');
+    $stat['record'] = Stat::record();
     Glob::diff('STAT_RECORD');
     // 禁用转向
     $redirect = true;
     if (preg_match("/^\/(robots|sitemap|play\/sitemap)(|\-\d+)\.(txt|xml|xml\.gz)$/i", $request_path)) {
         $redirect = false;
     }
-    $stat['enable_cookie'] = Stat::cookie($redirect, "ENABLE_COOKIE_$host_name", null);
+    $stat['cookie'] = Stat::cookie($redirect, "ENABLE_COOKIE_$host_name", null, Glob::$sid);
+    PhpRedis::db();
     Glob::diff('STAT_COOKIE');
 }
 
@@ -69,6 +87,7 @@ if (null !== $debug) {
     $files = get_included_files();
     // 合并漏掉的
     extract(\Ext\Yac::hash('files', 'files_', 1));
+    $cacheValue = is_array($cacheValue) ? $cacheValue : [];
     foreach ($cacheValue as $key => $value) {
         if (!in_array($value, $files)) {
             $files[] = $value;
