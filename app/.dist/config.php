e338eeb58642b2c58374735967ef86be27f975b1
wuding <netjoin@gmail.com>
2021-01-18 03:46:29 +0800

    新增配置文件示例

---
+++ app/.dist/config.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+    'database_connect' => 'database',
+    'database' => array(
+        'username' => '',
+        'password' => '',
+        'port' => 3306,
+    ),
+    'view' => array(
+        'cdn_prefix' => '',
+    ),
+);
