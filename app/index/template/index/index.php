40a05f9fd76232ce117e0f5997a81a1ec9f25dfd
wuding <netjoin@gmail.com>
2021-01-19 17:18:00 +0800

    搜索输入框自动聚焦、聚焦全选

--- app/index/template/index/index.php
+++ app/index/template/index/index.php
@@ -19,75 +19,16 @@
 </header>
 
 <form action="/search">
-    <input type="search" name="q" placeholder="搜索">
+    <input type="search" name="q" value="<?=$qh?>" placeholder="搜索" autofocus onfocus="this.select()">
     <blockquote style="display: none">
         <button type="submit">搜索</button>
     </blockquote>
 
     <div>
-        <dl>
-            <button type="submit" name="id" value="90091">
-                <dd><img src="/img/icon/google.ico"></dd>
-                <dt>Google</dt>
-            </button>
-        </dl>
-        <dl>
-            <button type="submit" name="id" value="40">
-                <dd><img class="fav-28" src="/img/icon/youtube.png"></dd>
-                <dt>YouTube</dt>
-            </button>
-        </dl>
-        <dl>
-            <button type="submit" name="id" value="43420">
-                <dd><img class="fav-30" src="https://www.amazon.com/favicon.ico"></dd>
-                <dt>Amazon</dt>
-            </button>
-        </dl>
-        <dl>
-            <button type="submit" name="id" value="76">
-                <dd><img class="fav-28" src="/img/icon/facebook.ico"></dd>
-                <dt>Facebook</dt>
-            </button>
-        </dl>
-        <dl>
-            <button type="submit" name="id" value="61">
-                <dd><img class="fav-28" src="https://www.bing.com/sa/simg/favicon-2x.ico"></dd>
-                <dt>Bing</dt>
-            </button>
-        </dl>
-        <dl>
-            <button type="submit" name="id" value="44">
-                <dd><img class="fav-28" src="https://yastatic.net/iconostasis/_/KKii9ECKxo3QZnchF7ayZhbzOT8.png"></dd>
-                <dt>Yandex</dt>
-            </button>
-        </dl>
-        <dl>
-            <button type="submit" name="id" value="431">
-                <dd><img class="fav-28" src="https://s3-media0.fl.yelpcdn.com/assets/public/favicon.yji-118ff475a341620f50dfbaddb83efb25.ico"></dd>
-                <dt>Yelp</dt>
-            </button>
-        </dl>
-        <dl>
-            <button type="submit" name="id" value="100">
-                <dd><img src="https://www.baidu.com/favicon.ico"></dd>
-                <dt>百度</dt>
-            </button>
-        </dl>
-        <dl>
-            <button type="submit" name="id" value="5090">
-                <dd><img class="fav-26" src="https://www.sogou.com/images/logo/new/favicon.ico?v=4"></dd>
-                <dt>搜狗</dt>
-            </button>
-        </dl>
-        <dl>
-            <button type="submit" name="id" value="360">
-                <dd><img class="fav-28" src="https://s2.ssl.qhres.com/static/121a1737750aa53d.ico"></dd>
-                <dt>360</dt>
-            </button>
-        </dl>
+        <?=$dl?>
         <dl>
             <a href="/search/settings/add">
-                <dd>+</dd>
+                <dd><img src="/img/icon/plus.png"></dd>
                 <dt>添加</dt>
             </a>
         </dl>
