71449b15f72c9d885dc8d9882285aa0f812342ce
wuding <netjoin@gmail.com>
2021-01-19 17:17:02 +0800

    可通过查询预设关键词，增加用户快捷搜索列表

--- app/index/controller/Index.php
+++ app/index/controller/Index.php
@@ -2,6 +2,8 @@
 
 namespace app\index\controller;
 
+use app\index\view\Prototype;
+use app\search\model\view\ShortcutKey;
 use function php\fn\{get, session};
 
 class Index extends \MagicCube\Controller
@@ -9,14 +11,33 @@ class Index extends \MagicCube\Controller
     public static function index()
     {
         //=s
+        // 查询
+        extract(get(array('q')));
+        // 会话
+        $uid = session('user.id', 1);
         $login = session('user.name');
+        // 模型
+        $ShortcutKey = new ShortcutKey;
+        // 其他
+        $hour = date('H');
+        $surplus = 24 - $hour;
 
         //=f
         $_SESSION['init'] = time();
+        // 视图
         $login_link = '<a href="/user/login">登录</a>';
         if ($login) {
             $login_link = '<a href="/user/settings">设置</a>';
         }
+
+        //=z
+        $qh = htmlspecialchars($q);
+
+        //=sh
+        // 模型
+        $all = $surplus ? $ShortcutKey->select('no, url, favicon, name', "user = '$uid'", 'id', $surplus) : array();
+        // 视图
+        $dl = Prototype::dl($all);
 
         //=g
         return get_defined_vars();
