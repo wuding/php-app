0b53b6ab0a80a8fdb0a9d8ce4aafd483e7296fbb
wuding <netjoin@gmail.com>
2021-02-09 15:51:46 +0800

    新增多语言配置示例

---
+++ conf/locale/.dist/en.php
@@ -0,0 +1,9 @@
+<?php
+/* Hash table */
+return array(
+    '076125b9' => 'URLNK.ORG',
+    'b6d4223e' => 'Sign in',
+    'f4f70727' => 'Settings',
+    '13348442' => 'Search',
+    'ec211f7c' => 'Add',
+);
