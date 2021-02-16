b6e8d910fd3e51db56e460b57f5fd351f09dee5f
wuding <netjoin@gmail.com>
2021-02-16 13:53:51 +0800

    新增代码复用特质类

---
+++ src/traits/Reuse.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace src\traits;
+
+trait Reuse
+{
+    // 翻译语言
+    public static function lang($lang = null, $func = null)
+    {
+        foreach ($lang as $key => $value) {
+            if (is_numeric($key)) {
+                $val = preg_replace("/\W+/", '_', $value);
+                $val = strtolower($val);
+                unset($lang[$key]);
+                $key = "_$val";
+            }
+            $lang[$key] = $func($value);
+        }
+        return $lang;
+    }
+}
