49a16c6f527b9e007c335c07293956183db7de4b
wuding <netjoin@gmail.com>
2021-08-06 15:14:34 +0800

    添加模块配置，补充键值

--- conf/.dist/config.php
+++ conf/.dist/config.php
@@ -13,7 +13,10 @@ return array(
                 'username' => '<USER>',
                 'password' => '<PASS>',
                 'port' => 3306,
-                'host' => 'localhost'
+                'host' => 'localhost',
+                'options' => array(
+                    PDO::ATTR_PERSISTENT => true,
+                ),
             ),
             'slave' => array(
                 'username' => '',
@@ -51,6 +54,7 @@ return array(
     'merge' => array(
         'server' => array(
             #'REMOTE_ADDR' => '',
+            #'REQUEST_URI' => '/music',
         ),
     ),
     // 地理位置
@@ -58,21 +62,96 @@ return array(
         // bin/country.bat 生成
         'country_uids' => array(
             'XA' => 1,
+            'AR' => 32,
+            'AU' => 36,
+            'MM' => 104,
+            'CA' => 124,
             'CN' => 156,
+            'TW' => 158,
+            'CY' => 196,
+            'CZ' => 203,
+            'FI' => 246,
+            'FR' => 250,
+            'DE' => 276,
+            'HK' => 344,
+            'IN' => 356,
+            'ID' => 360,
+            'IT' => 380,
+            'JP' => 392,
+            'KR' => 410,
+            'LT' => 440,
+            'MY' => 458,
+            'NL' => 528,
+            'PA' => 591,
+            'PL' => 616,
+            'RO' => 642,
+            'RU' => 643,
+            'SG' => 702,
+            'VN' => 704,
+            'ZA' => 710,
+            'ES' => 724,
+            'SE' => 752,
+            'UA' => 804,
+            'GB' => 826,
             'US' => 840,
+            'AP' => 922,
         ),
     ),
     // 本地化
     'locale' => array(
         // bin/translate.bat 生成翻译文件
         'func' => '\php\func\lang', // 或者 gettext
-        'domain' => '',
+        'domain' => '',#zh
         'directory' => ROOT .'/conf/locale',
         'default_language' => 'en',
         // bin/language.bat 生成语言对应国家地区
         'available_languages' => array(
+            'af' => 'ZA',
+            'ar' => 'EG',
+            'az' => 'AZ',
+            'be' => 'BY',
+            'bg' => 'BG',
+            'bo' => 'Xizang',
+            'ca' => 'ES',
+            'co' => 'FR',
+            'cs' => 'CZ',
+            'cy' => 'Wales',
+            'da' => 'DK',
+            'de' => 'DE',
             'en' => 'US',
             'eo' => 'Esperanto',
+            'es' => 'ES',
+            'fa' => 'IR',
+            'fil' => 'PH',
+            'fr' => 'FR',
+            'gl' => 'Galicia',
+            'hi' => 'IN',
+            'hu' => 'HU',
+            'id' => 'ID',
+            'it' => 'IT',
+            'ja' => 'JP',
+            'kk' => 'KZ',
+            'ko' => 'KR',
+            'la' => 'VA',
+            'ms' => 'MY',
+            'my' => 'MM',
+            'nb' => 'NO',
+            'ne' => 'NP',
+            'nl' => 'NL',
+            'no' => 'NO',
+            'pl' => 'PL',
+            'pt' => 'PT',
+            'ro' => 'RO',
+            'ru' => 'RU',
+            'so' => 'SO',
+            'sq' => 'AL',
+            'su' => 'ID',
+            'th' => 'TH',
+            'tr' => 'TR',
+            'ug' => 'Xinjiang',
+            'uk' => 'UA',
+            'und' => null,
+            'vi' => 'VI',
             'zh' => 'CN',
             'zh-hant' => 'TW',
         ),
@@ -114,6 +193,32 @@ return array(
     'ext' => array(
         'geoip' => array(
             'custom_directory' => ini_get('geoip.custom_directory') ?: ROOT .'/data/geoip',
+        ),
+    ),
+    // 模块
+    'module' => array(
+        '*' => array('id', 'index', 'music', 'note', 'notes', 'search', 'user'),
+        '/' => array('note', 'git',),
+        '_' => array(
+            'plural' => array(
+                's' => array(
+                    'n' => array('note'),
+                ),
+            ),
+        ),
+        'music' => array(
+            'theme' => 'appleMusic',
+        ),
+        'note' => array(
+            'alias' => 'notes',
+            'controller' => array(
+                'plural' => array(
+                    's' => array(
+                        'folder' => 302, // 0兼容 301永久转移 302转向
+                        'user', // 兼容
+                    ),
+                ),
+            ),
         ),
     ),
 );
