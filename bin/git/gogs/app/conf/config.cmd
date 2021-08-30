f955ea87c85f8f785173f7dd0dc86538c52bc8a5
wuding <netjoin@gmail.com>
2021-08-30 18:18:11 +0800

    新增同步 app 用户全局配置文件

---
+++ bin/git/gogs/app/conf/config.cmd
@@ -0,0 +1,7 @@
+SET drive_letter=L
+SET repo_path=L:\Server\SCM\Github\app-module\git\main
+SET log_file=temp\logs\%date:~0,4%%date:~5,2%%date:~8,2%.log
+SET git_path="C:\Program Files\Git\cmd\git.exe"
+SET branch_name=main
+SET remote_name=gogs
+SET remotes=git00,gitee,github
