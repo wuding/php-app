3f086dc04731dbee59666e3840fb9f049adc3bda
Benny <netjoin@gmail.com>
2023-06-05 17:53:26 +0800

    complement app/test/src/controller/Index VERSION srcDir enableView

--- app/test/src/controller/Index.php
+++ app/test/src/controller/Index.php
@@ -4,6 +4,10 @@ namespace app\test\src\controller;
 
 class Index extends \MagicCube\Controller
 {
+    const VERSION = '23.6.3';
+    public static $srcDir = 'src';
+    protected $enableView = true;
+
     public function __construct($vars = array())
     {
         if (is_numeric($vars['uriInfo']['controller'])) {
