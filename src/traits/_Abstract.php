56622449c216f20b650d8b5144ef2b73db82ec80
wuding <netjoin@gmail.com>
2024-06-06 15:56:30 +0800

    增加 JSON 编码选项参数

--- src/traits/_Abstract.php
+++ src/traits/_Abstract.php
@@ -7,13 +7,17 @@ use Ext\Zlib;
 
 trait _Abstract
 {
-    public static function _outputJson($code = null, $msg = null, $data = null)
+    public static function _outputJson($code = null, $msg = null, $data = null, $var_array = null)
     {
+        $options = 0;
+        if (is_array($var_array)) {
+            extract($var_array);
+        }
         $arr = [
             'code' => $code,
             'msg' => $msg,
             'data' => $data
         ];
-        return json_encode($arr);
+        return json_encode($arr, $options);
     }
 }
