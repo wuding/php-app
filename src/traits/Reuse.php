d85b054af9b1a5f45108632152023a204f3adf3e
wuding <netjoin@gmail.com>
2024-06-06 15:55:16 +0800

    complement src\traits\Reuse getModuleConfigFilename getAppPathname

--- src/traits/Reuse.php
+++ src/traits/Reuse.php
@@ -18,4 +18,45 @@ trait Reuse
         }
         return $lang;
     }
+
+    /*
+    address
+    */
+
+    // module
+    // config.php
+    public static function getModuleConfigFilename($class = '', $file = 'config.php')
+    {
+        $root = constant('ROOT');
+        $pathname = self::getAppPathname($class);
+        $arr = array($root, $pathname, $file);
+        // $filename = $pathname .'/'. $file;
+        $filename = implode('/', $arr);
+        return $filename;
+        var_dump([__FILE__, __LINE__,  get_defined_vars()]);exit;
+    }
+
+    public static function getAppPathname($class = '', $patter = 'src|controller')
+    {
+        $arr = explode('\\', $class);
+        $pattern = "/^(". $patter .")$/";
+        // $pattern = "/(src|controller)/";
+
+        $dirs = array();
+        foreach ($arr as $key => $value) {
+            // if ('src' === $value) {
+            if (preg_match($pattern, $value)) {
+                break;
+            }
+            $dirs[] = $value;
+        }
+
+        $str = implode('/', $dirs);
+        return $str;
+        var_dump([__FILE__, __LINE__, $arr, $dirs, $str]);exit;
+    }
+
+    /*
+    content
+    */
 }
