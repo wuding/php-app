9b869db3507cef5bfe8c5ec91cf4e2ce9bdea048
wuding <netjoin@gmail.com>
2025-02-10 10:39:20 +0800

    bin/php/fu-app_60144.bat

---
+++ bin/php/fu-app_60144.bat
@@ -0,0 +1,5 @@
+J:
+cd J:\git\github.com\wuding\php-app\future\web
+php -S 0.0.0.0:60144
+::I:\System\ProgramFiles\php\php-8.0.30-nts\php -S 0.0.0.0:8081 router.php
+::"O:\System\ProgramFiles(x86)\RunHiddenConsole.exe" php -S 0.0.0.0:60917
