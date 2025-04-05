7e8f224a918161a7e846cb2bdeaddd79187f2af8
wuding <netjoin@gmail.com>
2025-04-05 18:52:32 +0800

    web/layout.php

---
+++ web/layout.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+    <style type="text/css">
+        html {
+            background: white;
+            padding: 10px;
+        }
+        body {
+            display: block;
+            margin: 0 auto;
+            background: red;
+            height: 17400px;
+            width: 100%;
+/*            max-height: 1740px;*/
+            max-width: 1240px;
+        }
+    </style>
+</head>
+<body>
+<header>header</header>
+
+<main>mian</main>
+
+<footer>footer</footer>
+</body>
+</html>
