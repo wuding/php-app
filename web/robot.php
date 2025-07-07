348f641b07307f08e66c2c5ad40abed08e966401
wuding <netjoin@gmail.com>
2025-07-08 03:35:14 +0800

    Conducting /robot.php <input> init_url

--- web/robot.php
+++ web/robot.php
@@ -1,6 +1,6 @@
 <!doctype html>
 <html>
-<!-- version 20250620.3 -->
+<!-- version 20250708.4 -->
 <?php
 $url = $_GET['url'] ?? null;
 $millisec = $_GET['millisec'] ?? null;
@@ -20,7 +20,7 @@ $last = $_GET['last'] ?? null;
     <legend>请求</legend>
     请求地址 <input id="url" name="request_url" value="<?=$url ?? null?>" style="width:650px">
     <button type="button" onclick="start()" id="btn-request">开始</button>
-    <button type="button" onclick="document.getElementById('item').value='';start();">更改</button>
+    <button type="button" onclick="reset()">更改</button>
     <p>
     请求键名 <input id="item" name="request_key" value="<?=$item?>" style="width:650px">
     <button type="button" onclick="recovery(1)">恢复</button>
@@ -31,6 +31,9 @@ $last = $_GET['last'] ?? null;
         <option></option>
     </select>
     <p>
+    初始地址 <input id="init_url" value="" style="width:650px">
+    <button type="button" onclick="restore()">还原</button>
+    <p>
     请求延时 <input id="timeout" name="delay" value="1" style="width:200px">
     <p>
     最大页码 <input id="max_page" name="maximum" value="" style="width:200px">
@@ -39,8 +42,17 @@ $last = $_GET['last'] ?? null;
     <legend>计划</legend>
     任务代码 <button type="button" onclick="autoTask()" id="btn-task">开始</button>
     <p>
-    <textarea id="code" style="width:100%">testTask()</textarea>
-    <p>间隔毫秒 <input id="millisec" name="interval" value="<?=($millisec ?? null) ? : 1000?>" style="width:200px">
+    <textarea id="code" style="width:100%" placeholder="任务代码">retry();</textarea>
+    <p>
+        <textarea style="width:100%" disabled rows="5">
+restart();
+retry();
+start(1);start(0);
+testTask();
+        </textarea>
+    <p>
+        <textarea style="width:100%" placeholder="暂存文本"></textarea>
+    <p>间隔毫秒 <input id="millisec" name="interval" value="<?=($millisec ?? null) ? : 900000?>" style="width:200px">
     <p>重复次数 <input id="repeat_time" name="repeat" value="" style="width:200px">
        运行次数 <input id="run_times" value="" style="width:200px" disabled>
     <p>设置时间 <input id="config_time" value="" style="width:350px" disabled>
@@ -67,6 +79,7 @@ $last = $_GET['last'] ?? null;
     <legend>消息</legend>
     <div id="msg"></div>
     <div id="note"></div>
+    <textarea id="result_code" style="width:100%" rows="10"></textarea>
 </fieldset>
 <fieldset>
 <legend>回收</legend>
@@ -81,13 +94,13 @@ $last = $_GET['last'] ?? null;
     重置地址 <input type="text" name="" value="<?=$url ?? null?>" id="gc_url" style="width:650px">
 </p>
 <p>
-    最后地址 <input type="text" name="" value="<?=$last ?? null?>" id="gc_url_last" style="width:650px">
+    最后地址 <input type="text" name="" value="<?=$last ?? null?>" id="gc_url_last" style="width:650px" disabled>
 </p>
 <p>
     重置时间 <input id="reset_time" value="" style="width:350px" disabled>
 </p>
 </fieldset>
-<script type="text/javascript" src="/js/robot.js?v=20250620.4"></script>
+<script type="text/javascript" src="/js/robot.js?v=20250708.6"></script>
 <script type="text/javascript">
 <?php
 if ($start ?? null) {
