aa03eae6832b573bff59903096d2f146f68c9135
wuding <netjoin@gmail.com>
2026-02-24 16:27:09 +0800

    4= $0 bin/php/17400;php-app_next.bat

---
+++ bin/php/17400;php-app_next.bat
@@ -0,0 +1,6 @@
+F:
+cd F:\Server\http\php-app_v26\web
+php -S 0.0.0.0:17400 index.php
+:: php -S 0.0.0.0:60144 router.php 2>>out.log
+::I:\System\ProgramFiles\php\php-8.0.30-nts\php -S 0.0.0.0:8081 router.php
+::"O:\System\ProgramFiles(x86)\RunHiddenConsole.exe" php -S 0.0.0.0:60917
