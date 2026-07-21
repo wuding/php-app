a59d534886943f159b40668569e367147d540cb2
wuding <netjoin@gmail.com>
2026-07-21 21:45:30 +0800

    21= $ Conducts index.php Func ocr()

--- web/index.php
+++ web/index.php
@@ -7,18 +7,20 @@ use MagicCube\Dispatcher;
 use NewUI\Engine;
 use Ext\X\Redis as PhpRedis;
 
+// ocr
+// use thiagoalessio\TesseractOCR\TesseractOCR;
+
 class Router
 {
   // verb
   const BUILD = 20260206.173855;
   const EDITION = 3.30;
-  const REVISION = 2;
-  const VERSION = 26.0322;
+  const REVISION = 3;
+  const VERSION = 26.0721;
 
   function __construct()
   {
     self::init();
-    // include 'J:/http/php-app/vendor/wuding/php-func/src/Func.php';
   }
 
   static function autoload($filename = null)
@@ -30,24 +32,12 @@ class Router
 
   static function init()
   {
-    $constants = [
-      'ROOT' => dirname(__DIR__),
-    ];
-    $psr4 = [
-/*      'Dew\\' => ROOT .'/src/dew/',
-      'Frost\\' => ROOT .'/src/frost/',
-      'Snow\\' => ROOT .'/src/snow/',
-      'Ice\\' => ROOT .'/src/ice/',
-      'Func\\' => ROOT .'/src/func/',
-      'Ext\\' => ROOT .'/src/ext/',
-      'Pkg\\' => ROOT .'/src/pkg/',*/
-    ];
-    $includes = [];
-
     $var_array = include '../conf/develop.php';
-    extract($var_array);
+    $constants = $var_array['constants'] ?? [];
+    $psr4 = $var_array['psr4'] ?? [];
+    $includes = $var_array['includes'] ?? [];
 
-    $err = [];
+    $err = $a = $b = [];
     foreach ($constants as $name => $value) {
       if (defined($name)) {
         $err[$name] = constant($name);
@@ -57,41 +47,46 @@ class Router
     }
 
     $autoload = self::autoload();
-    $a = [];
     foreach ($psr4 as $key => $value) {
-      $a[] = $autoload->addPsr4($key, $value);
+      $a[$key] = $autoload->addPsr4($key, $value);
     }
-
-    $b = [];
     foreach ($includes as $key => $value) {
       $b[$key] = include $value;
     }
+
+    // 导入配置
+    // Glob::$conf = $var_array;
+    new Glob($var_array);
+    $timezoneId = Glob::conf('ini.date.timezone');
+    $tz = $timezoneId ? date_default_timezone_set($timezoneId) : 'nul';
+    // $tzs = date_default_timezone_get();
+    // print_r(get_defined_vars());
     return $err;
   }
 
   static function run()
   {
     global $template;
-    // 导入配置
-    Glob::$conf = include ROOT .'/conf/develop.php';
     $redis_conf = Glob::cnf('mem.alias.connect', 'redis') ?? array();
     $mem = Glob::set('Mem', new PhpRedis($redis_conf));
+    $needle = Glob::conf('debug.run', true);
 
     $uri = self::uri();
     $debug = $_GET['debug'] ?? null;
     $vars = self::vars($uri);
+    $haystack = explode(',', $debug);
 
     // 控制器、模板
     $nsTpl = "app\{m}{src}{extra}\controller\{c}";
     $ns = str_replace('{src}', $vars['srcDir'], $nsTpl);
     $extra = "\\theme\{t}";
 
+    $template = new Engine();
     new Dispatcher($uri, Glob::class, $vars['prefix'], $vars['srcDir']);
     $obj = Dispatcher::dispatch($debug, $ns, $extra, $_GET ? null : $_SERVER['REQUEST_URI']);
-    $template = new Engine();
 
     // 调试
-    if ($debug) {
+    if (in_array($needle, $haystack, true)) {
         // print_r(get_defined_vars());
         print_r(array(__FILE__, __LINE__));
         print_r(get_included_files());
@@ -105,15 +100,12 @@ class Router
     $srcDir = '';
 
     $array = explode('/', $uri);
+    // var_dump([$array, $uri]);
     list(, $module) = $array;
     $module_names = [
       // 'index',
     ];
-    $module_folders = [
-      'app',
-      'api',
-      'calendar',
-    ];
+    $module_folders = Glob::conf('modules');
 
     $module = strtolower($module) ?: 'index';
     if ($module && $module_names && !in_array($module, $module_names)) {
@@ -142,9 +134,28 @@ class Router
 
     $dirname = '\\' === $dirname ? '' : $dirname;
     $uri = $dirname ."/". $filename;
-    return $uri = preg_replace("/^\/\//", '/', $uri);
+    $uri = preg_replace("/^\/\//", '/', $uri);
+    $uri = preg_replace("/^\/index/", '', $uri);
+    $pattern = "#(search|watch|play)#";
+    if (preg_match($pattern, $uri, $matches)) {
+        $uri = "/index$uri";
+    }
+    // $uri = rtrim($uri, '/');
+    return $uri;
+  }
+
+  static function ocr()
+  {
+    $text = (new TesseractOCR('path/to/image.png'))
+    ->lang('eng') // language, you can also use other traineddata if installed
+    ->run();
+
+echo $text;
   }
 }
 
+// ksort($_SERVER);
+// print_r($GLOBALS);
 $Router = new Router();
 return Router::run();
+// return Router::ocr();
